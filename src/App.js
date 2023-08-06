1fd69062ac04ed429b13257e7d1c8c1a453265de
akashdomadiya <akashdomadiya2003@gmail.com>
2023-08-06 21:32:24 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Counter } from './features/counter/Counter';
+import ProductList from './features/Product-list/ProductList';
+import Home from './pages/Home';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+
+    <div className="App">
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import ProductList from "../features/Product-list/ProductList";
+import NavBar from "../features/navbar/navbar";
+
+function Home(){
+    return(
+        <div>
+            <NavBar>
+                <ProductList>
+                    
+                </ProductList>
+            </NavBar>
+        </div>
+
+    );
+}
+export default Home;
